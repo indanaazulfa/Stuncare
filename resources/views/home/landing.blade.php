6521d445a977609c383647f82dda4706e78fb3b5
indanaazulfa <indana_zulfa@student.ub.ac.id>
2023-12-01 11:31:47 +0700

    menghubungkan ke halaman login

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -59,7 +59,7 @@
           <h2 data-aos="fade-up" data-aos-delay="400">Membangun Generasi Unggul Tanpa Stunting</h2>
           <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
-              <a href="login.html" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+              <a href="/login" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                 <span>Masuk</span>
                 <i class="bi bi-arrow-right"></i>
               </a>
