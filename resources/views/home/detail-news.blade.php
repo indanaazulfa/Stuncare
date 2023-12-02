dc010010b5de17282d5076b7ff57e7d3fd7208c0
eiko farah <eikofarahdiva@student.ub.ac.id>
2023-12-02 16:58:01 +0700

    ubah logo tag

--- resources/views/home/detail-news.blade.php
+++ resources/views/home/detail-news.blade.php
@@ -5,13 +5,13 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Blog Single - FlexStart Bootstrap Template</title>
+  <title>Detail News</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ asset('assets/img/Tag.png') }}" rel="icon">
+  <link href="{{ asset('assets/img/Tag.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -50,7 +50,7 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto" href="/home">Home</a></li>
-          <li><a class="nav-link scrollto" href="/informasi">Informasi</a></li>
+          <li><a class="nav-link scrollto" href="/news">Informasi</a></li>
           <li><a class="nav-link scrollto" href="{{ route('logout') }}">Logout</a></li>
         <i class="mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
